c913b7445b5d8a27e1132b3a53de10989007a6fd
NischalKeshav <nischalkeshav.official@gmail.com>
2026-02-10 20:56:05 -0600

    read func

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,15 +1,9 @@
 org 0x7C00
 bits 16
-
-
-
 %define ENDL 0x00, 0x0A
 
-;FAT12 header
 jmp short start
 nop
-
-
 bdb_oem: db 'MSWIN4.1'
 bdb_bytes_per_sector: dw 512
 bdb_sector_per_cluster: db 1
@@ -23,8 +17,6 @@ bdb_sectors_per_track: dw 18
 bdb_heads: dw 2
 bdb_hidden_sectors: dd 0
 bdb_large_sector_count: dd 0
-
-
 edr_drive_number: db 0
 		  db 0
 ebr_signature:    db 29h
@@ -32,8 +24,23 @@ ebr_volume_id:    dd 67676767h
 ebr_volume_label: db 'Urahara    '
 ebr_system_id: db 'FAT12   '
 
+floppy_error:
+	mov si, msg_read_fail
+	call puts
+	jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+	mov ah, 0
+	int 16h
+	jmp 0FFFFh:0
+
+halt:
+	cli
+	hlt
+
 start:
 	jmp main
+
 puts:
 	push si
 	push ax
@@ -41,40 +48,94 @@ puts:
 	lodsb
 	or al, al
 	jz .done
-
 	mov ah, 0x0e
 	mov bh, 0
 	int 0x10
-
 	jmp .loop
-
-
 .done:
-	pop ax;
-	pop si;
-	ret;
-
-
+	pop ax
+	pop si
+	ret
 
 main:
-	mov ax, 0;
+	mov ax, 0
 	mov ds, ax
 	mov es, ax
 	mov ss, ax
 	mov sp, 0x7C00
+	mov [edr_drive_number], dl
+	mov ax, 1
+	mov cl, 1
+	mov bx, 0x7E00
+	call disk_read
 	mov si, msg_hello
 	call puts
+	cli
 	hlt
-
 .halt:
 	jmp .halt
 
-msg_hello: db "Hello World",ENDL, 0
+lba_to_chs:
+	push ax
+	push dx
+	xor dx, dx
+	div word [bdb_sectors_per_track]
+	inc dx
+	mov cx, dx
+	xor dx, dx
+	div word [bdb_heads]
+	mov dh, dl
+	mov ch, al
+	shl ah, 6
+	or cl, ah
+	pop ax
+	mov dl, al
+	pop ax
+	ret
 
+disk_read:
+	push ax
+	push bx
+	push cx
+	push dx
+	push di
+	push cx
+	call lba_to_chs
+	pop ax
+	mov ah, 02h
+	mov di, 3
+.retry:
+	pusha
+	stc
+	int 13h
+	jnc .done
+	popa
+	call disk_reset
+	dec di
+	test di, di
+	jnz .retry
+.fail:
+	jmp floppy_error
+.done:
+	popa
+	pop di
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	ret
+
+disk_reset:
+	pusha
+	mov ah, 0
+	stc
+	int 13h
+	jc floppy_error
+	popa
+	ret
+
+msg_hello: db "Hello World", ENDL, 0
+msg_read_fail: db "Reading Failed", ENDL, 0
 
 times 510 -($-$$) db 0
 dw 0AA55h
-
-
-
-
